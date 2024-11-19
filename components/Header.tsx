6afb9acdc47ee534e9ec251952ab67b2bf9eeb8e
Exequiel Lobaiza <51865273+exelobaiza@users.noreply.github.com>
2024-11-18 19:41:00 -0500

    Update Header.tsx
    new class navBar

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,7 @@ import { Button } from 'components/ui/button'
 export function Header() {
   return (
     <header className="border-b bg-white">
-      <div className="max-w-6xl mx-auto px-4 py-4">
+      <div className="max-w-7xl mx-auto px-4 py-4">
         <img 
           src="https://hebbkx1anhila5yf.public.blob.vercel-storage.com/logo-wallbit-fe8a4ef0-rVX4Xq56Eqgxl6UPh3uQLOOBzijwHe.svg"
           alt="Wallbit"
